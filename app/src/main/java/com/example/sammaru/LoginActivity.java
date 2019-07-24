5861a7cc008898407df7306028a151c2b0ff4dea
Yanjoo <hanjub3@gmail.com>
2019-07-24 20:59:12 +0900

    기사님 메인 프래그먼트 추가

--- app/src/main/java/com/example/sammaru/LoginActivity.java
+++ app/src/main/java/com/example/sammaru/LoginActivity.java
@@ -2,14 +2,10 @@ package com.example.sammaru;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import com.example.sammaru.fragment.DeliveryJoinFragment;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -33,8 +29,8 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // MainActivity 실행
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                // DeliveryMainActivity 실행
+                startActivity(new Intent(LoginActivity.this, DeliveryMainActivity.class));
                 finish();
             }
         });
