6a1adb6cf31bd133f4c6ba5c3b0fbe45b2717b41
mkk0754 <rkdalsrb1212@naver.com>
2019-07-29 23:38:06 +0900

    setting 브랜치

--- app/src/main/java/com/example/sammaru/SplashActivity.java
+++ app/src/main/java/com/example/sammaru/SplashActivity.java
@@ -16,7 +16,7 @@ import com.example.sammaru.login.LoginActivity;
  * Google Firebase 연동 필요
  * */
 
-// 강민규 test
+// setting 브랜치
 public class SplashActivity extends AppCompatActivity {
 
     @Override
