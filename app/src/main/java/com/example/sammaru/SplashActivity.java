88d0fe5a237bd28e66624bf0ff5142f5fdbed959
mkk0754 <rkdalsrb1212@naver.com>
2019-07-29 23:36:32 +0900

    delivery check 브랜치

--- app/src/main/java/com/example/sammaru/SplashActivity.java
+++ app/src/main/java/com/example/sammaru/SplashActivity.java
@@ -16,7 +16,7 @@ import com.example.sammaru.login.LoginActivity;
  * Google Firebase 연동 필요
  * */
 
-// 강민규 test
+// delivery check
 public class SplashActivity extends AppCompatActivity {
 
     @Override
