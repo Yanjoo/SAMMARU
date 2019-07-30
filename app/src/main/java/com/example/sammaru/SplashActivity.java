eb97d40dd3e200fb525831ece4eb9cbb967d49c3
Yanjoo <hanjub3@gmail.com>
2019-07-30 17:49:42 +0900

    chat test 2

--- app/src/main/java/com/example/sammaru/SplashActivity.java
+++ app/src/main/java/com/example/sammaru/SplashActivity.java
@@ -16,8 +16,8 @@ import com.example.sammaru.login.LoginActivity;
  * Google Firebase 연동 필요
  * */
 
-// chat branch test
-  
+// chat branch test 2
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
