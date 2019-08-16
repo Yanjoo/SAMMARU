d812b29a8536a6d4dfa64706977804233dc7d76e
yanjoo <gkswn14520>
2019-08-16 16:49:26 +0900

    서버에 택배 정보 저장

--- app/src/main/java/com/example/sammaru/customer/DeliveryListFragment.java
+++ app/src/main/java/com/example/sammaru/customer/DeliveryListFragment.java
@@ -4,17 +4,27 @@ package com.example.sammaru.customer;
 import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.sammaru.R;
+import com.example.sammaru.model.ProductModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
@@ -26,8 +36,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class DeliveryListFragment extends Fragment {
 
     private FloatingActionButton fab;
-    private String delivery_company = null;
-    private String getDelivery_normalset = "https://tracker.delivery/#/";
+    private String deliveryCompany = null;
+    private String baseUrl = "https://tracker.delivery/#/";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -52,35 +62,34 @@ public class DeliveryListFragment extends Fragment {
         builder.setView(view);
 
         Spinner spinner = view.findViewById(R.id.dialog_lookup_add_spinner);
-
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
                 switch (position){
-                    case 0: delivery_company ="de.dhl/"; break;
-                    case 1: delivery_company ="kr.chunilps/"; break;
-                    case 2: delivery_company ="kr.cjlogistics/"; break;
-                    case 3: delivery_company ="kr.cupost/"; break;
-                    case 4: delivery_company ="kr.cvsnet/"; break;
-                    case 5: delivery_company ="kr.cway/"; break;
-                    case 6: delivery_company ="kr.daesin/"; break;
-                    case 7: delivery_company ="kr.epost/"; break;
-                    case 8: delivery_company ="kr.hanips/"; break;
-                    case 9: delivery_company ="kr.hanjin/"; break;
-                    case 10: delivery_company ="kr.hdexp/"; break;
-                    case 11: delivery_company ="kr.homepick/"; break;
-                    case 12: delivery_company ="kr.honamlogis/"; break;
-                    case 13: delivery_company ="kr.ilyanglogis/"; break;
-                    case 14: delivery_company ="kr.kdexp/"; break;
-                    case 15: delivery_company ="kr.kunyoung/"; break;
-                    case 16: delivery_company ="kr.logen/"; break;
-                    case 17: delivery_company ="kr.lotte/"; break;
-                    case 18: delivery_company ="kr.slx/"; break;
-                    case 19: delivery_company ="nl.tnt/"; break;
-                    case 20: delivery_company ="un.upu.ems/"; break;
-                    case 21: delivery_company ="us.fedex/"; break;
-                    case 22: delivery_company ="us.ups/"; break;
-                    case 23: delivery_company ="us.usps/"; break;
+                    case 0: deliveryCompany ="de.dhl/"; break;
+                    case 1: deliveryCompany ="kr.chunilps/"; break;
+                    case 2: deliveryCompany ="kr.cjlogistics/"; break;
+                    case 3: deliveryCompany ="kr.cupost/"; break;
+                    case 4: deliveryCompany ="kr.cvsnet/"; break;
+                    case 5: deliveryCompany ="kr.cway/"; break;
+                    case 6: deliveryCompany ="kr.daesin/"; break;
+                    case 7: deliveryCompany ="kr.epost/"; break;
+                    case 8: deliveryCompany ="kr.hanips/"; break;
+                    case 9: deliveryCompany ="kr.hanjin/"; break;
+                    case 10: deliveryCompany ="kr.hdexp/"; break;
+                    case 11: deliveryCompany ="kr.homepick/"; break;
+                    case 12: deliveryCompany ="kr.honamlogis/"; break;
+                    case 13: deliveryCompany ="kr.ilyanglogis/"; break;
+                    case 14: deliveryCompany ="kr.kdexp/"; break;
+                    case 15: deliveryCompany ="kr.kunyoung/"; break;
+                    case 16: deliveryCompany ="kr.logen/"; break;
+                    case 17: deliveryCompany ="kr.lotte/"; break;
+                    case 18: deliveryCompany ="kr.slx/"; break;
+                    case 19: deliveryCompany ="nl.tnt/"; break;
+                    case 20: deliveryCompany ="un.upu.ems/"; break;
+                    case 21: deliveryCompany ="us.fedex/"; break;
+                    case 22: deliveryCompany ="us.ups/"; break;
+                    case 23: deliveryCompany ="us.usps/"; break;
                     default: break;
                 }
             }
@@ -90,8 +99,39 @@ public class DeliveryListFragment extends Fragment {
             }
         });
 
+        final EditText number = view.findViewById(R.id.dialog_lookup_add_editText);
+
         final AlertDialog dialog = builder.create();
         dialog.setTitle("택배 추가");
+
+        Button add = view.findViewById(R.id.dialog_lookup_add_button_add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 서버에 저장할 데이터
+                ProductModel productModel = new ProductModel();
+                productModel.setUrl(baseUrl + deliveryCompany + number.getText().toString());
+
+                String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                productModel.setUid(uid);
+
+                int startIndex = deliveryCompany.indexOf('.') + 1;
+                int endIndex = deliveryCompany.indexOf('/');
+                String companyName = deliveryCompany.substring(startIndex, endIndex);
+
+                Log.d("DeliveryListFragment", companyName);
+
+                FirebaseDatabase.getInstance().getReference().child("products").child(companyName).child(uid).push().setValue(productModel)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(getContext(), "등록 성공!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                dialog.dismiss();
+            }
+        });
 
         Button cancel = view.findViewById(R.id.dialog_lookup_add_button_cancel);
         cancel.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/example/sammaru/model/ProductModel.java
@@ -0,0 +1,49 @@
+package com.example.sammaru.model;
+
+public class ProductModel {
+    private String productName;
+    private String url;
+    private String owner;
+    private String address;
+    private String uid;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
