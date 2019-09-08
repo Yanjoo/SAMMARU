70a4dedbc1b7d5a1f021677de1d48514e2518d7d
KangMinkyu <mkk0754>
2019-09-08 11:53:29 +0900

    구글 로그인 제거

--- app/src/main/java/com/example/sammaru/login/LoginActivity.java
+++ app/src/main/java/com/example/sammaru/login/LoginActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -17,18 +16,10 @@ import com.example.sammaru.R;
 import com.example.sammaru.courier.CourierMainActivity;
 import com.example.sammaru.customer.CustomerMainActivity;
 import com.example.sammaru.model.UserModel;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -48,15 +39,6 @@ public class LoginActivity extends AppCompatActivity {
     private int loginIdentifier;    // 로그인 구분자 1:고객, 2:배달원
 
     private SharedPreferences sharedPreferences;    // 이메일 기억에 사용
-    // 구글로그인 result 상수
-    private static final int RC_SIGN_IN = 900;
-
-    // 구글api클라이언트
-    private GoogleSignInClient googleSignInClient;
-
-
-    // 구글  로그인 버튼
-    private SignInButton buttonGoogle;
 
 
     @Override
@@ -100,24 +82,7 @@ public class LoginActivity extends AppCompatActivity {
         // 파이어베이스 인증 객체 선언
         firebaseAuth = FirebaseAuth.getInstance();
 
-        buttonGoogle = findViewById(R.id.activity_login_button_googleSignIn);
 
-        // Google 로그인을 앱에 통합
-        // GoogleSignInOptions 개체를 구성할 때 requestIdToken을 호출
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
-
-        buttonGoogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent signInIntent = googleSignInClient.getSignInIntent();
-                startActivityForResult(signInIntent, RC_SIGN_IN);
-            }
-        });
     }
 
     // loginEvent : 서버에서 id, pw 검사 후 고객, 배달원 구분해서 mainActivity 실행
@@ -136,14 +101,12 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             final String uid = task.getResult().getUser().getUid();
 
-                            Log.d("LoginActivity ", uid);
                             databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
                                 @Override
                                 public void onDataChange(DataSnapshot dataSnapshot) {
                                     for (DataSnapshot item : dataSnapshot.getChildren()) {
                                         UserModel userModel = item.getValue(UserModel.class);
-                                        Log.d("LoginActivity ", userModel.toString());
-                                        Log.d("LoginActivity ", userModel.getUid());
+
                                         Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
                                         if (userModel.getUid().equals(uid)) {
                                             loginIdentifier = userModel.getIdentifier(); // 구분자 1: 고객, 2: 배달원
@@ -228,40 +191,4 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // 구글로그인 버튼 응답
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                // 구글 로그인 성공
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                firebaseAuthWithGoogle(account);
-
-            } catch (ApiException e) {
-
-            }
-        }
-    }
-
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        firebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // 로그인 성공
-                            Toast.makeText(LoginActivity.this, "로그인 성공", Toast.LENGTH_SHORT).show();
-                        } else {
-                            // 로그인 실패
-                            Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
-                        }
-
-                    }
-                });
-    }
 }
